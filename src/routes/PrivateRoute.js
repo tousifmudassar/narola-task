692140b5df4435cdead27041196a16f10bf58934
mudassar tousif <tousifmudassar@gmail.com>
2020-08-26 17:48:03 +0530

    Header Change

--- src/routes/PrivateRoute.js
+++ src/routes/PrivateRoute.js
@@ -1,14 +1,18 @@
-import React, {Component} from 'react';
-import {Route, Redirect} from 'react-router-dom';
-import Cookies from 'js-cookie';
+import React, { Component } from "react";
+import { Route, Redirect } from "react-router-dom";
+import Cookies from "js-cookie";
 
-const PrivateRoute = ({component: Component, ...rest}) => {
-    let isAuthenticated = Cookies.get('token') && true;
+const PrivateRoute = ({ component: Component, ...rest }) => {
+  let isAuthenticated = Cookies.get("token") && true;
 
-    return <Route 
-        {...rest}
-        render = {props => isAuthenticated ? <Component {...props} /> : <Redirect to="/" />}
+  return (
+    <Route
+      {...rest}
+      render={(props) =>
+        isAuthenticated ? <Component {...props} /> : <Redirect to="/" />
+      }
     />
-}
+  );
+};
 
 export default PrivateRoute;
