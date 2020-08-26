96311be80b1f94bc231d508adfeb184113ce9a57
mudassar tousif <tousifmudassar@gmail.com>
2020-08-26 17:25:17 +0530

    Header change

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,32 +1,49 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import {Row,Col, Navbar,Collapse, NavbarBrand,Nav,NavItem,NavLink} from 'reactstrap';
+import React from "react";
+import { Link } from "react-router-dom";
+import {
+  Row,
+  Col,
+  Navbar,
+  Collapse,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+} from "reactstrap";
 
 const Header = () => {
-    return (
-        <Row>
-            <Col>
-                <Navbar color="light" light expand="md">
-                    <Link to="/" className="navbar-brand">Todo App</Link>
-                    <Collapse navbar>
-                        <Nav navbar className="ml-auto">
-                            <NavItem>
-                                <Link className="nav-link" to="/dashboard">Dashboard</Link>
-                            </NavItem>
+  return (
+    <Row>
+      <Col>
+        <Navbar color="light" light expand="md">
+          <Link to="/" className="navbar-brand">
+            GND-Todo App
+          </Link>
+          <Collapse navbar>
+            <Nav navbar className="ml-auto">
+              <NavItem>
+                <Link className="nav-link" to="/dashboard">
+                  Dashboard
+                </Link>
+              </NavItem>
 
-                            <NavItem>
-                                <Link className="nav-link" to="/profile">Profile</Link>
-                            </NavItem>
+              <NavItem>
+                <Link className="nav-link" to="/profile">
+                  Profile
+                </Link>
+              </NavItem>
 
-                            <NavItem>
-                                <Link className="nav-link" to="/logout">Logout</Link>
-                            </NavItem>
-                        </Nav>
-                    </Collapse>
-                </Navbar>
-            </Col>
-        </Row>
-    )
-}
+              <NavItem>
+                <Link className="nav-link" to="/logout">
+                  Logout
+                </Link>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Col>
+    </Row>
+  );
+};
 
-export default Header; 
+export default Header;
